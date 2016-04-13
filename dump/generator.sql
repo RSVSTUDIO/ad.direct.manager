826871c54fd2767d3cfed5bf22fcfaebc223f2c7
Denis <sysadm85@gmail.com>
2016-04-13 21:34:57 +0600

    dump

---
+++ dump/generator.sql
@@ -0,0 +1,329 @@
+-- MySQL dump 10.13  Distrib 5.6.28, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: seo_admin
+-- ------------------------------------------------------
+-- Server version	5.6.28-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `generator_settings`
+--
+
+DROP TABLE IF EXISTS `generator_settings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `generator_settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `shop_id` int(11) NOT NULL,
+  `price_from` double DEFAULT NULL,
+  `price_to` double DEFAULT NULL,
+  `brands` text,
+  PRIMARY KEY (`id`),
+  KEY `FK_generator_settings_shops` (`shop_id`),
+  CONSTRAINT `FK_generator_settings_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `generator_settings`
+--
+
+LOCK TABLES `generator_settings` WRITE;
+/*!40000 ALTER TABLE `generator_settings` DISABLE KEYS */;
+INSERT INTO `generator_settings` VALUES (1,1,10,10000,'6,7,13,18');
+/*!40000 ALTER TABLE `generator_settings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `shop_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `title` varchar(500) NOT NULL,
+  `seo_title` varchar(33) NOT NULL,
+  `keywords` text NOT NULL,
+  `price` float NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_available` tinyint(1) NOT NULL,
+  `yandex_campaign_id` bigint(20) NOT NULL,
+  `yandex_adgroup_id` bigint(20) NOT NULL,
+  `yandex_ad_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_products_shops` (`shop_id`),
+  CONSTRAINT `FK_products_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `settings`
+--
+
+DROP TABLE IF EXISTS `settings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `value` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `settings`
+--
+
+LOCK TABLES `settings` WRITE;
+/*!40000 ALTER TABLE `settings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `settings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shops`
+--
+
+DROP TABLE IF EXISTS `shops`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shops` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `brand_api_url` text NOT NULL,
+  `product_api_url` text NOT NULL,
+  `api_secret_key` varchar(50) NOT NULL,
+  `yandex_application_id` varchar(50) NOT NULL,
+  `yandex_secret` varchar(50) NOT NULL,
+  `yandex_access_token` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shops`
+--
+
+LOCK TABLES `shops` WRITE;
+/*!40000 ALTER TABLE `shops` DISABLE KEYS */;
+INSERT INTO `shops` VALUES (1,'Paramount','http://paramount-shop.dev/api/brands','http://paramount-shop.dev/api/products','secretPas$w0rd','ab69b628b4b54f99ae70e690cdea7934','7fe1ae7f993a45bd80b342b057e28303','df815cc852914128a32d73e892f5e3b4');
+/*!40000 ALTER TABLE `shops` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_queue`
+--
+
+DROP TABLE IF EXISTS `task_queue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_queue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `shop_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `started_at` datetime NOT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `operation` varchar(50) DEFAULT NULL,
+  `completed_at` datetime DEFAULT NULL,
+  `context` text,
+  `error` text,
+  PRIMARY KEY (`id`),
+  KEY `status` (`status`),
+  KEY `FK_task_queue_shops` (`shop_id`),
+  CONSTRAINT `FK_task_queue_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_queue`
+--
+
+LOCK TABLES `task_queue` WRITE;
+/*!40000 ALTER TABLE `task_queue` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task_queue` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `templates`
+--
+
+DROP TABLE IF EXISTS `templates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `templates` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `shop_id` int(10) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `message` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_templates_shops` (`shop_id`),
+  CONSTRAINT `FK_templates_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `templates`
+--
+
+LOCK TABLES `templates` WRITE;
+/*!40000 ALTER TABLE `templates` DISABLE KEYS */;
+INSERT INTO `templates` VALUES (1,1,'Продам [vendor] по цене [price]','dfghg'),(3,1,'[title]','Продам [title]');
+/*!40000 ALTER TABLE `templates` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `password_hash` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Денис','sysadm85@gmail.com','qimus','$2y$13$o7HsIhaq4D75M5HvcyLVUe7rViKf9AoCvSrtKgI7OTOjIgHfpqP1S');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `yandex_campaign`
+--
+
+DROP TABLE IF EXISTS `yandex_campaign`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `yandex_campaign` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `shop_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `yandex_id` bigint(20) NOT NULL,
+  `products_count` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `FK_yandex_campaign_shops` (`shop_id`),
+  KEY `brand_id` (`brand_id`),
+  CONSTRAINT `FK_yandex_campaign_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `yandex_campaign`
+--
+
+LOCK TABLES `yandex_campaign` WRITE;
+/*!40000 ALTER TABLE `yandex_campaign` DISABLE KEYS */;
+/*!40000 ALTER TABLE `yandex_campaign` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `yandex_oauth`
+--
+
+DROP TABLE IF EXISTS `yandex_oauth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `yandex_oauth` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `shop_id` int(11) NOT NULL,
+  `access_token` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_oauth_users` (`user_id`),
+  KEY `FK_oauth_shops` (`shop_id`),
+  CONSTRAINT `FK_oauth_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_oauth_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `yandex_oauth`
+--
+
+LOCK TABLES `yandex_oauth` WRITE;
+/*!40000 ALTER TABLE `yandex_oauth` DISABLE KEYS */;
+INSERT INTO `yandex_oauth` VALUES (1,1,1,'df815cc852914128a32d73e892f5e3b4');
+/*!40000 ALTER TABLE `yandex_oauth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `yandex_update_log`
+--
+
+DROP TABLE IF EXISTS `yandex_update_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `yandex_update_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_id` int(11) NOT NULL,
+  `shop_id` int(11) NOT NULL,
+  `entity_type` varchar(50) NOT NULL,
+  `entity_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `operation` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `message` varchar(300) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_yandex_update_log_shops` (`shop_id`),
+  KEY `task_id` (`task_id`),
+  CONSTRAINT `FK_yandex_update_log_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_yandex_update_log_task_queue` FOREIGN KEY (`task_id`) REFERENCES `task_queue` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `yandex_update_log`
+--
+
+LOCK TABLES `yandex_update_log` WRITE;
+/*!40000 ALTER TABLE `yandex_update_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `yandex_update_log` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-13 21:33:47
